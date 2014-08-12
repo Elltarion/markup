4d1e82f2defac53a270a6242016dae7ab7ee2963
Elltarion <miroshnichenkomikhail94@gmail.com>
2014-08-12 10:35:19 +0400

    ROOT V ROT

--- js/script.js
+++ js/script.js
@@ -2,6 +2,23 @@
 	$(document).on('ready', function(){
 		
 		var ajaxControllerPath = '';
+				
+		/* Опросы  */
+		
+		$('.survey').on('change', 'input', function(){
+			var parent = $(this).parents('.module')
+			parent.toggleClass('module-loading')
+			$.post(ajaxControllerPath, {
+				element: 'survey',
+				data: { id: $(this).attr('id'), value: $(this).val() }
+			}).done(function(){
+				parent.toggleClass('module-loading')
+				parent.find('ul').remove()
+				parent.find('span').text('Спасибо, Ваш голос учтен!')
+			})
+		})
+		
+		/* PLUGINS */
 		
 		var width = screen.width-20;
 		var slider = $('.showcase ul')
@@ -21,31 +38,23 @@
 				slider.find('li:not(.bx-clone)').eq(1).clone().addClass('fake').appendTo('.showcase ul')				
 			}
 		})
-		/* Опросы  */
-		$('.survey').on('change', 'input', function(){
-			var parent = $(this).parents('.module')
-			parent.toggleClass('module-loading')
-			$.post(ajaxControllerPath, {
-				element: 'survey',
-				data: { id: $(this).attr('id'), value: $(this).val() }
-			}).done(function(){
-				parent.toggleClass('module-loading')
-				parent.find('ul').remove()
-				parent.find('span').text('Спасибо, Ваш голос учтен!')
-			})
-		})
 		
 		$('.video').fancybox({
 			type: 'iframe'
 		})
 		
+		$('.scroll ul').perfectScrollbar({
+			suppressScrollX:true,
+			includePadding:true
+		})		
+		var $container = $('.module.inspire .wrap').masonry({
+				columnWidth: 100,
+				gutter: 20,
+				itemSelector: 'img'
+			})
+		
 		VK.Widgets.Group("vk_groups_1", {mode: 0, width: "370", height: "300", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 20003922)		
 		VK.Widgets.Group("vk_groups_2", {mode: 0, width: "370", height: "300", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 20003922)		
-		
-		var js, fjs = document.getElementsByTagName('script')[0];
-		js = document.createElement('script'); js.id = 'facebook-jssdk';
-		js.src = "http://connect.facebook.net/ru_RU/sdk.js#xfbml=1&appId=814052025293438&version=v2.0";
-		fjs.parentNode.insertBefore(js, fjs)
-
 	})
+	
 })(jQuery)
